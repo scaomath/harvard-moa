9af76990c6d6cd2299ff8aae1281208afa3ef822
S.Cao <scao.math@gmail.com>
2020-11-20 22:53:53 -0600

    update data retrieving.

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+#%%
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 import numpy as np
 import random
@@ -147,5 +148,212 @@ def process_data(data):
     
     return data
 
+
+#%%
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+    
+data_dir = '/home/scao/Documents/harvard-moa'
+train_features = pd.read_csv(data_dir+'/data/train_features.csv')
+train_targets_scored = pd.read_csv(data_dir+'/data/train_targets_scored.csv')
+train_targets_nonscored = pd.read_csv(data_dir+'/data/train_targets_nonscored.csv')
+
+test_features = pd.read_csv(data_dir+'/data/test_features.csv')
+sample_submission = pd.read_csv(data_dir+'/data/sample_submission.csv')
+
+#%% GENES PCA
+GENES = [col for col in train_features.columns if col.startswith('g-')]
+CELLS = [col for col in train_features.columns if col.startswith('c-')]
+
+n_comp = 29
+
+data = pd.concat([pd.DataFrame(train_features[GENES]), pd.DataFrame(test_features[GENES])])
+data2 = (PCA(n_components=n_comp, random_state=42).fit_transform(data[GENES]))
+train2 = data2[:train_features.shape[0]]; test2 = data2[-test_features.shape[0]:]
+
+train2 = pd.DataFrame(train2, columns=[f'pca_G-{i}' for i in range(n_comp)])
+test2 = pd.DataFrame(test2, columns=[f'pca_G-{i}' for i in range(n_comp)])
+
+# drop_cols = [f'c-{i}' for i in range(n_comp,len(GENES))]
+train_features = pd.concat((train_features, train2), axis=1)
+test_features = pd.concat((test_features, test2), axis=1)
+
+#CELLS
+n_comp = 4
+
+data = pd.concat([pd.DataFrame(train_features[CELLS]), pd.DataFrame(test_features[CELLS])])
+data2 = (PCA(n_components=n_comp, random_state=42).fit_transform(data[CELLS]))
+train2 = data2[:train_features.shape[0]]; test2 = data2[-test_features.shape[0]:]
+
+train2 = pd.DataFrame(train2, columns=[f'pca_C-{i}' for i in range(n_comp)])
+test2 = pd.DataFrame(test2, columns=[f'pca_C-{i}' for i in range(n_comp)])
+
+# drop_cols = [f'c-{i}' for i in range(n_comp,len(CELLS))]
+train_features = pd.concat((train_features, train2), axis=1)
+test_features = pd.concat((test_features, test2), axis=1)
+
+#%% variance thresh
+var_thresh = VarianceThreshold(threshold=0.7)
+data = train_features.append(test_features)
+data_transformed = var_thresh.fit_transform(data.iloc[:, 4:])
+
+train_features_transformed = data_transformed[ : train_features.shape[0]]
+test_features_transformed = data_transformed[-test_features.shape[0] : ]
+
+
+train_features = pd.DataFrame(train_features[['sig_id','cp_type','cp_time','cp_dose']].values.reshape(-1, 4),\
+                              columns=['sig_id','cp_type','cp_time','cp_dose'])
+
+train_features = pd.concat([train_features, pd.DataFrame(train_features_transformed)], axis=1)
+
+
+test_features = pd.DataFrame(test_features[['sig_id','cp_type','cp_time','cp_dose']].values.reshape(-1, 4),\
+                             columns=['sig_id','cp_type','cp_time','cp_dose'])
+
+test_features = pd.concat([test_features, pd.DataFrame(test_features_transformed)], axis=1)
+
+train = train_features.merge(train_targets_scored, on='sig_id')
+train = train[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
+test = test_features[test_features['cp_type']!='ctl_vehicle'].reset_index(drop=True)
+
+target = train[train_targets_scored.columns]
+
+train = train.drop('cp_type', axis=1)
+test = test.drop('cp_type', axis=1)
+
+#%%
+target_cols = target.drop('sig_id', axis=1).columns.values.tolist()
+folds = train.copy()
+
+mskf = MultilabelStratifiedKFold(n_splits=3)
+
+for f, (t_idx, v_idx) in enumerate(mskf.split(X=train, y=target)):
+    folds.loc[v_idx, 'kfold'] = int(f)
+
+folds['kfold'] = folds['kfold'].astype(int)
+feature_cols = [c for c in process_data(folds).columns if c not in target_cols]
+feature_cols = [c for c in feature_cols if c not in ['kfold','sig_id']]
+
+#%%
+DEVICE = ('cuda' if torch.cuda.is_available() else 'cpu')
+EPOCHS = 20
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-3
+WEIGHT_DECAY = 1e-5
+NFOLDS = 3
+EARLY_STOPPING_STEPS = 11
+EARLY_STOP = True
+
+num_features=len(feature_cols)
+num_targets=len(target_cols)
+hidden_size=1024
+
+#%%
+def run_training(fold, seed):
+    get_seed(seed)
+    train = process_data(folds)
+    test_ = process_data(test)
+    
+    trn_idx = train[train['kfold'] != fold].index
+    val_idx = train[train['kfold'] == fold].index
+    
+    train_df = train[train['kfold'] != fold].reset_index(drop=True)
+    valid_df = train[train['kfold'] == fold].reset_index(drop=True)
+    
+    x_train, y_train  = train_df[feature_cols].values, train_df[target_cols].values
+    x_valid, y_valid =  valid_df[feature_cols].values, valid_df[target_cols].values
+    
+    train_dataset = MoADataset(x_train, y_train)
+    valid_dataset = MoADataset(x_valid, y_valid)
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    validloader = torch.utils.data.DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    
+    model = Model(
+        num_features=num_features,
+        num_targets=num_targets,
+        hidden_size=hidden_size,
+    )
+    
+    model.to(DEVICE)
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+    scheduler = optim.lr_scheduler.OneCycleLR(optimizer=optimizer, pct_start=0.05, div_factor=1.5e3, 
+                                              max_lr=1e-2, epochs=EPOCHS, steps_per_epoch=len(trainloader))
+    
+    loss_fn = nn.BCEWithLogitsLoss()
+    
+    early_stopping_steps = EARLY_STOPPING_STEPS
+    early_step = 0
+    
+    oof = np.zeros((len(train), target.iloc[:, 1:].shape[1]))
+    best_loss = np.inf
+    
+    for epoch in range(EPOCHS):
+        
+        train_loss = train_fn(model, optimizer,scheduler, loss_fn, trainloader, DEVICE)
+        print(f"FOLD: {fold}, EPOCH: {epoch}, train_loss: {train_loss}")
+        valid_loss, valid_preds = valid_fn(model, loss_fn, validloader, DEVICE)
+        print(f"FOLD: {fold}, EPOCH: {epoch}, valid_loss: {valid_loss}")
+        
+        if valid_loss < best_loss:
+            
+            best_loss = valid_loss
+            oof[val_idx] = valid_preds
+            torch.save(model.state_dict(), f"FOLD{fold}_.pth")
+        
+        elif(EARLY_STOP == True):
+            
+            early_step += 1
+            if (early_step >= early_stopping_steps):
+                break
+            
+    
+    x_test = test_[feature_cols].values
+    testdataset = TestDataset(x_test)
+    testloader = torch.utils.data.DataLoader(testdataset, batch_size=BATCH_SIZE, shuffle=False)
+    
+    model = Model(
+        num_features=num_features,
+        num_targets=num_targets,
+        hidden_size=hidden_size,
+    )
+    
+    model.load_state_dict(torch.load(f"FOLD{fold}_.pth"))
+    model.to(DEVICE)
+    
+    predictions = np.zeros((len(test_), target.iloc[:, 1:].shape[1]))
+    predictions = inference_fn(model, testloader, DEVICE)
+    
+    return oof, predictions
+
+def run_k_fold(NFOLDS, seed):
+    oof = np.zeros((len(train), len(target_cols)))
+    predictions = np.zeros((len(test), len(target_cols)))
+    
+    for fold in range(NFOLDS):
+        oof_, pred_ = run_training(fold, seed)
+        
+        predictions += pred_ / NFOLDS
+        oof += oof_
+        
+    return oof, predictions
+
+def main():
+    SEED = [42]
+    oof = np.zeros((len(train), len(target_cols)))
+    predictions = np.zeros((len(test), len(target_cols)))
+
+    for seed in SEED:
+        oof_, predictions_ = run_k_fold(NFOLDS, seed)
+        oof += oof_ / len(SEED)
+        predictions += predictions_ / len(SEED)
+
+    train[target_cols] = oof
+    test[target_cols] = predictions
+    return oof, predictions
+#%%
 if __name__ == "__main__":
     get_seed(seed=1127)
+    main()
+   
+# %%
